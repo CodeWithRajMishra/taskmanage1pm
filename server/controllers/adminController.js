3890d2d35d8ccd871fe84c327257ddd124a48f87
CodeWithRajMishra <rajmishra3@gmail.com>
2025-11-03 15:00:51 +0530

    resume

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -1,35 +1,46 @@
 const AdminModel = require("../models/adminModel");
-const UserPassword= require("../middlewares/randomPassword");
-const adminLogin=async(req, res)=>{
+const UserPassword = require("../middlewares/randomPassword");
+const emailSend = require("../middlewares/empMailSen");
+const EmpModel = require("../models/empModel");
+
+const adminLogin = async (req, res) => {
     const { email, password } = req.body;
     try {
-         const Admin = await AdminModel.findOne({email:email});
-        
-         if (!Admin)
-         {
-            res.status(401).send({msg:"Invalid Email ID"});
-         }
-         
-         if (Admin.password!=password){
-             res.status(401).send({msg:"Invalid Password"});
-         }
+        const Admin = await AdminModel.findOne({ email: email });
 
-         res.status(200).send({Admin:Admin,msg:"Succesfully Login"});
+        if (!Admin) {
+            res.status(401).send({ msg: "Invalid Email ID" });
+        }
+
+        if (Admin.password != password) {
+            res.status(401).send({ msg: "Invalid Password" });
+        }
+
+        res.status(200).send({ Admin: Admin, msg: "Succesfully Login" });
 
     } catch (error) {
-         console.log(error);
+        console.log(error);
     }
 }
 
 
-const userCreate=async(req, res)=>{
-    const {empname,empemail,designation} = req.body;
-    console.log(UserPassword.myPassword());
-    res.send("OKK");
+const userCreate = async (req, res) => {
+    const { empname, empemail, designation } = req.body;
+    const emppassword = UserPassword.myPassword();
+    // console.log(UserPassword.myPassword());
+    emailSend.userMailsender(empname, empemail, emppassword);
 
+    const Employee = await EmpModel.create({
+
+        name: empname,
+        email: empemail,
+        designation: designation,
+        password: emppassword
+    })
+    res.status(201).send("user succesfully created!!!");
 }
 
-module.exports ={
+module.exports = {
     adminLogin,
     userCreate
 }
